19865d3eed0a0ba1701e3ca15b77d1e30719bbdc
陈锐 <chenrui@chenruideMacBook-Pro.local>
2015-08-15 16:21:24 +0800

    init

---
+++ OcDemo/OcDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OcDemo
+//
+//  Created by 陈锐 on 15/8/15.
+//  Copyright (c) 2015年 陈锐. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
